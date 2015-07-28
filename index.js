bec7bc8e9e6313a26ecf4413a4f20a48150ec12b
Chuck Pierce <chuck@percolate.com>
2015-07-28 13:10:39 -0400

    have server and webpack watch working

--- index.js
+++ index.js
@@ -1,53 +1,66 @@
-require('./github')
+var webpackConfig = require('./config').webpack
 var gui = require('nw.gui')
-// var win = gui.Window.get()
-// var exec = require('child_process').exec
-// var cmd = 'cd ~/percolate/demo/ui && grunt watch'
-// var psTree = require('ps-tree')
-//
-// var child = exec(cmd)
-//
-// child.stdout.on('data', function(data) {
-//     console.log('stdout: ' + data);
-// })
-// child.stderr.on('data', function(data) {
-//     console.log('stderr: ' + data);
-// })
-// child.on('close', function(code) {
-//     console.log('closing code: ' + code);
-// })
-//
-// var kill = function (pid, signal, callback) {
-//     signal   = signal || 'SIGINT';
-//     callback = callback || function () {}
-//     var killTree = true;
-//     if(killTree) {
-//         psTree(pid, function (err, children) {
-//             var pidArr = []
-//             pidArr.push(pid)
-//             pidArr.concat(
-//                 children.map(function (p) {
-//                     return p.PID;
-//                 })
-//             ).forEach(function (tpid) {
-//                 try { process.kill(tpid, signal) }
-//                 catch (ex) { }
-//             })
-//             callback()
-//         });
-//     } else {
-//         try { process.kill(pid, signal) }
-//         catch (ex) { }
-//         callback()
-//     }
-// }
-//
-// win.on('close', function () {
-//
-//     var stop = exec('cd ~/percolate/demo/ui && grunt gae:stop')
-//     stop.on('close', function() {
-//         console.log('closed')
-//         kill(child.pid)
-//         win.close(true)
-//     })
-// })
+var win = gui.Window.get()
+var exec = require('child_process').exec
+var path = require('path')
+var webpack = require('webpack')
+var cmd = '/usr/local/google_appengine/dev_appserver.py ~/percolate/demo'
+var psTree = require('ps-tree')
+
+var child = exec(cmd)
+
+var root = path.resolve(gui.App.dataPath, '../../..')
+
+
+child.stdout.on('data', function(data) {
+    console.log('stdout: ' + data)
+})
+child.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
+})
+child.on('close', function(code) {
+    console.log('closing code: ' + code);
+})
+
+var config = require(path.join(root, '/percolate/demo/ui/lib/config'))
+
+var compiler = webpack(webpackConfig)
+
+compiler.watch({
+    aggregateTimeout: 300,
+    poll: true
+}, function(err, stats) {
+    console.log('Change has been made', stats)
+});
+
+var kill = function (pid, signal, callback) {
+    signal   = signal || 'SIGINT';
+    callback = callback || function () {}
+    var killTree = true;
+    if(killTree) {
+        psTree(pid, function (err, children) {
+            var pidArr = []
+            pidArr.push(pid)
+            pidArr.concat(
+                children.map(function (p) {
+                    return p.PID;
+                })
+            ).forEach(function (tpid) {
+                try { process.kill(tpid, signal) }
+                catch (ex) { }
+            })
+            callback()
+        });
+    } else {
+        try { process.kill(pid, signal) }
+        catch (ex) { }
+        callback()
+    }
+}
+
+win.on('close', function () {
+    kill(child.pid, 'SIGINT', function() {
+        console.log('killed')
+        win.close(true)
+    })
+})
