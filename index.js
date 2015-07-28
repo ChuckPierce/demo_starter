1a1b8d4d9f5675f40a156fa4846ec93fafd0ee8f
Chuck Pierce <chuck@percolate.com>
2015-07-28 15:08:10 -0400

    comment on index file

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 var gui = require('nw.gui')
 var win = gui.Window.get()
+
 var exec = require('child_process').exec
 var path = require('path')
 var webpack = require('webpack')
-var cmd = '/usr/local/google_appengine/dev_appserver.py ~/percolate/demo'
-var psTree = require('ps-tree')
 
+// command to execute python development server with demo directory
+var cmd = '/usr/local/google_appengine/dev_appserver.py ~/percolate/demo'
+
+// create child process to start server and set maxBuffer to handle preloading assets
 var child = exec(cmd,{maxBuffer:200*10024})
-var count = 0
+//set root for OSX machines
 var root = path.resolve(gui.App.dataPath, '../../..')
 
-
+// handle dev console logs
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data)
 })
 child.stderr.on('data', function(data) {
     console.log('stderr: ' + data);
-    count++
 })
 child.on('close', function(code, signal) {
     console.log('closing code: ' + code + ' ' + signal);
-    console.log(count)
 })
 
+// start webpack watch for changes on js files when changing branches
 var config = require(path.join(root, '/percolate/demo/ui/lib/config'))
-
 var compiler = webpack(config.webpack)
-
 compiler.watch({
     aggregateTimeout: 300,
     poll: true
@@ -34,6 +34,7 @@ compiler.watch({
     console.log('Change has been made', stats)
 })
 
+// kill process on window close
 win.on('close', function () {
     child.kill('SIGTERM')
     win.close(true)
