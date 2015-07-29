b84ac74340d312f49cbddfdbaac0d73ae9573025
Chuck Pierce <chuck@percolate.com>
2015-07-29 15:45:24 -0400

    create debugger

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ win.menu = nativeMenuBar;
 var server = require('./server')
 var watch = require('./watch')
 var watcher
+var DEBUG
 
 var message = document.querySelector('.message')
 var watchBtn = document.querySelector('.watch')
 var serverBtn = document.querySelector('.server')
-
+var debugBtn = document.querySelector('.debug-btn')
+var debuggerConsole = document.querySelector('.debugger')
 // run server
 server.run(message, function () {
     // run watch
-    watcher = watch.run(message, gui)
+    watcher = watch.run(message, gui, document)
 })
 // server button click
 serverBtn.addEventListener('click', function () {
     server.kill('SIGTERM')
     message.textContent = 'Server has stopped'
-    if(!watcher) watcher = watch.run(message, gui)
+    if(!watcher) watcher = watch.run(message, gui, document)
     watcher.close(function() {
         server.run(message, function () {
             watcher = watch.run(message, gui)
@@ -34,8 +36,20 @@ serverBtn.addEventListener('click', function () {
 watchBtn.addEventListener('click', function () {
     watcher.close(function () {
         message.textContent = "restarting watch"
-        watcher = watch.run(message, gui)
+        watcher = watch.run(message, gui, document)
     })
+})
+
+debugBtn.addEventListener('click', function () {
+    if (DEBUG) {
+        debuggerConsole.style.visibility = 'hidden'
+        debugBtn.textContent = 'Show Debugger'
+        DEBUG = false
+    } else {
+        debuggerConsole.style.visibility = 'visible'
+        debugBtn.textContent = 'Hide Debugger'
+        DEBUG = true
+    }
 })
 
 // kill process on window close
--- watch.js
+++ watch.js
@@ -5,7 +5,7 @@ var jsWatcher = {
     compiler: undefined,
     watchStarted: false,
     watcher: undefined,
-    run: function (message, gui) {
+    run: function (message, gui, document) {
         // set root for OSX machines
         var root = path.resolve(gui.App.dataPath, '../../..')
 
@@ -19,6 +19,26 @@ var jsWatcher = {
             aggregateTimeout: 300,
             poll: true
         }, function(err, stats) {
+            if (err) {
+                console.log(err)
+            }
+
+            var node = document.createElement('P')
+            var debuggerConsole = document.querySelector('.debugger')
+            if (stats.compilation.errors.length > 0) {
+                stats.compilation.errors.forEach(function (error) {
+                    var textNode = document.createTextNode(error.message)
+                    node.appendChild(textNode)
+                    node.style.color = 'red'
+                    debuggerConsole.insertBefore(node, debuggerConsole.childNodes[0])
+                })
+            } else {
+                var textNode = document.createTextNode('Compile successful')
+                node.appendChild(textNode)
+                node.style.color = 'green'
+                debuggerConsole.insertBefore(node, debuggerConsole.childNodes[0])
+            }
+
             if (this.watchStarted) {
                 message.textContent = 'A change has been made...still watching'
             } else {
